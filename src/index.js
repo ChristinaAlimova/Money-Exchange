a8f76183622c07b27847ae33605dd78d96016d7b
Christina Alimova <alimova.christina@gmail.com>
2018-09-21 22:24:49 +0300

    5-th task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,57 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var result = {};
+    var H = 50;
+    var Q = 25;
+    var D = 10;
+    var N = 5; 
+    var P = 1;
+    var divide;
+    var percent;
+    var obj = {};
+    
+
+    if (currency <= 0) {
+        return result;
+    }
+
+    if (currency >= 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+
+    divide = currency / H;
+    divide = parseInt(divide);
+    if (divide > 0) {
+        obj["H"] = divide;
+    }
+
+    percent = currency % H;
+    divide = percent / Q;
+    divide = parseInt(divide);
+    if (divide > 0) {
+        obj["Q"] = divide;
+    }
+
+    percent = percent % Q;
+    divide = percent / D;
+    divide = parseInt(divide);
+    if (divide > 0) {
+        obj["D"] = divide;
+    }
+
+    percent = percent % D;
+    divide = percent / N;
+    divide = parseInt(divide);
+    if (divide > 0) {
+        obj["N"] = divide;
+    }
+
+    percent = percent % N;
+    divide = percent / P;
+    divide = parseInt(divide);
+    if (divide > 0) {
+        obj["P"] = divide;
+    }
+
+    return obj;
 }
